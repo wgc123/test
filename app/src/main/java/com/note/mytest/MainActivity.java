8d75a657eeb953b5b40e9f23337e08b6e0dcc5c4
wangguicai <786722510@qq.com>
2020-09-25 10:31:55 +0800

    第一次提交

---
+++ app/src/main/java/com/note/mytest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.note.mytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ceshi
+
+    }
+}
---
+++ testlibrary/src/main/java/com/note/testlibrary/tool/MyTest.java
@@ -0,0 +1,4 @@
+package com.note.testlibrary.tool;
+
+public class MyTest {
+}
