3e7b0f03224dbfec94e94f3d52ab8c14348b8c6c
wangguicai <786722510@qq.com>
2020-09-29 11:03:04 +0800

    BroadcastReceiver 使用

---
+++ testlibrary/src/main/java/com/note/testlibrary/tool/MyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.note.testlibrary.tool;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 广播 BroadcastReceiver
+ * @author wgc
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
