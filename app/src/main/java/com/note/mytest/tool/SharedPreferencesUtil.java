6eea08983deeaf39394cded3266b186498995c9c
wangguicai <786722510@qq.com>
2020-11-12 09:36:53 +0800

    判断是否第一次进入欢迎页

---
+++ app/src/main/java/com/note/mytest/tool/SharedPreferencesUtil.java
@@ -0,0 +1,31 @@
+package com.note.mytest.tool;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * 判断是否第一次进入欢迎页
+ * @author wgc
+ */
+public class SharedPreferencesUtil {
+
+    private static final String spFileName = "welcomePage";
+    public static final String FIRST_OPEN = "first_open";
+
+    public static Boolean getBoolean(Context context, String strKey, Boolean strDefault) {
+        SharedPreferences setPreferences = context.getSharedPreferences(
+                spFileName, Context.MODE_PRIVATE);
+        Boolean result = setPreferences.getBoolean(strKey, strDefault);
+        return result;
+    }
+
+    public static void putBoolean(Context context, String strKey,
+                                  Boolean strData) {
+        SharedPreferences activityPreferences = context.getSharedPreferences(
+                spFileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = activityPreferences.edit();
+        editor.putBoolean(strKey, strData);
+        editor.commit();
+    }
+
+}
